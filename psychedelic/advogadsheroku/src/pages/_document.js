d93dcad279a47dce1a53d2fe23e44c8c47875e9d
Lumaraescoder <mephsluh@gmail.com>
2020-12-30 16:13:10 -0500

    comitado

--- psychedelic/advogadsheroku/src/pages/_document.js
+++ psychedelic/advogadsheroku/src/pages/_document.js
@@ -12,7 +12,20 @@ class MyDocument extends Document {
         <Head>
         
           <title>Psychedeliclab</title>
-          
+          <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-CTY634NDSF"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+                      
+              gtag('config', 'G-CTY634NDSF');
+            </script>
+              `,
+          }}
+          />
             <meta charSet={"utf-8"}/>
 
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
