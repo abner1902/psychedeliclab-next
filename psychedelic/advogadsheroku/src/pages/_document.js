c898a448048703ca21f9a243a31deaa3672a3b5b
Lumaraescoder <mephsluh@gmail.com>
2020-12-30 14:49:54 -0500

    metamap

--- psychedelic/advogadsheroku/src/pages/_document.js
+++ psychedelic/advogadsheroku/src/pages/_document.js
@@ -10,16 +10,19 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-
-          
+        
           <title>Psychedeliclab</title>
+          
             <meta charSet={"utf-8"}/>
+
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <meta name="og:site_name" content="Psychedelic lab"/>
             <meta name="fb:page_id" content="Psychedelic lab"/>
             <meta name="og:description" content="Darkpsy"/>
             <meta name="description" content="Psychedelic lab, aprenda a produzir Darkpsy com a primeira plataforma de cursos brasileiras focada na vertente"/>
             <link rel="shortcut icon" href="images/favicon.ico" type="favicon.ico"/>
+            <meta name="language" content="pt-br, en"/>
+
         </Head>
         <body>
           <Main />
