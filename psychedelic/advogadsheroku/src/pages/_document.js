2031e789fbe3ad6cdcac8361f72591f087574172
Lumaraescoder <mephsluh@gmail.com>
2020-12-30 16:51:14 -0500

    metaog2

--- psychedelic/advogadsheroku/src/pages/_document.js
+++ psychedelic/advogadsheroku/src/pages/_document.js
@@ -29,7 +29,7 @@ class MyDocument extends Document {
             <meta charSet={"utf-8"}/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <meta name="og:title" content="Curso Darkpsy Psychedliclab" />
-            <meta name="og:image" content="https://psychedelilab.com/darkpsy.jpeg" />
+            <meta name="og:image" content="https://psychedeliclab.com.br/darkpsy.jpeg" />
             <meta name="og:site_name" content="Psychedelic lab"/>
             <meta name="fb:page_id" content="Psychedelic lab"/>
             <meta name="og:description" content="Darkpsy"/>
