1db31a441d89f9c9afa97edc083a3538f9e3c183
Lumaraescoder <mephsluh@gmail.com>
2021-01-05 09:13:13 -0500

    seo

--- psychedelic/advogadsheroku/src/pages/_document.js
+++ psychedelic/advogadsheroku/src/pages/_document.js
@@ -78,11 +78,14 @@ class MyDocument extends Document {
         <meta name="fb:page_id" content="Psychedelic lab" />
         <meta property="og:type" content="Course" />
         <meta name="og:description" content="Darkpsy" />
-        <meta name="twitter:card" content="summary" />
         <meta name="description" content="Psychedelic lab, aprenda a produzir Darkpsy com a primeira plataforma de cursos brasileira focada na vertente." />
         <meta property="og:title" content="Curso Produção Darkpsy" />
         <meta property="og:description" 
               content="Do zero ao profissional" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Darkpsy course" />
+        <meta name="twitter:image" content="https://www.psychedeliclab.com.br/darkpsy.jpeg" />
+
         <body>
           <Main />
           <NextScript />
