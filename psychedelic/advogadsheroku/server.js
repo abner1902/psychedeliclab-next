d3c24ced3c28e1ec44d5696d98a0be7a1091e9cc
Lumaraescoder <mephsluh@gmail.com>
2021-01-12 00:28:02 -0500

    node

--- psychedelic/advogadsheroku/server.js
+++ psychedelic/advogadsheroku/server.js
@@ -1,27 +1,76 @@
-const { createServer } = require('http')
-const { parse } = require('url')
+ 
+const express = require('express')
 const next = require('next')
+const path = require('path')
+const url = require('url')
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = process.env.PORT || 3000
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+// Multi-process to utilize all CPU cores.
+if (!dev && cluster.isMaster) {
+  console.log(`Node cluster master ${process.pid} is running`)
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  }).listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`)
   })
-})
+} else {
+  const nextApp = next({ dir: '.', dev })
+  const nextHandler = nextApp.getRequestHandler()
+
+  nextApp.prepare()
+    .then(() => {
+      const server = express()
+
+      if (!dev) {
+        // Enforce SSL & HSTS in production
+        server.use(function (req, res, next) {
+          var proto = req.headers['x-forwarded-proto']
+          if (proto === 'https') {
+            res.set({
+              'Strict-Transport-Security': 'max-age=31557600' // one-year
+            })
+            return next()
+          }
+          res.redirect('https://' + req.headers.host + req.url)
+        })
+      }
+
+      // Static files
+      // https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
+      server.use('/static', express.static(path.join(__dirname, 'static'), {
+        maxAge: dev ? '0' : '365d'
+      }))
+
+      // Example server-side routing
+      server.get('/a', (req, res) => {
+        return nextApp.render(req, res, '/b', req.query)
+      })
+
+      // Example server-side routing
+      server.get('/b', (req, res) => {
+        return nextApp.render(req, res, '/a', req.query)
+      })
+
+      // Default catch-all renders Next app
+      server.get('*', (req, res) => {
+        // res.set({
+        //   'Cache-Control': 'public, max-age=3600'
+        // });
+        const parsedUrl = url.parse(req.url, true)
+        nextHandler(req, res, parsedUrl)
+      })
+
+      server.listen(port, (err) => {
+        if (err) throw err
+        console.log(`Listening on http://localhost:${port}`)
+      })
+    })
+}
