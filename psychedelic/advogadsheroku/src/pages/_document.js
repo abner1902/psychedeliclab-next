04315b419881cef0c203e2670c986fc9a1ce40ca
Lumaraescoder <mephsluh@gmail.com>
2021-01-11 21:11:39 -0500

    asd

--- psychedelic/advogadsheroku/src/pages/_document.js
+++ psychedelic/advogadsheroku/src/pages/_document.js
@@ -31,13 +31,11 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-CTY634NDSF"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());        
-              gtag('config', 'G-CTY634NDSF');
-            </script>
+              <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WJ8XGSQ');</script>
               `
             }}
           />
@@ -104,6 +102,8 @@ class MyDocument extends Document {
         <meta name="twitter:image" content="https://www.psychedeliclab.com.br/darkpsy.jpeg" />
 
         <body>
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJ8XGSQ"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
           <Main />
           <NextScript />
         </body>
