5b6342bf1c7c2966ca0be42f63a36809dd4bf441
Lumaraescoder <mephsluh@gmail.com>
2021-01-15 19:40:14 -0500

    performace

--- psychedelic/advogadsheroku/src/components/tutorials/index.js
+++ psychedelic/advogadsheroku/src/components/tutorials/index.js
@@ -11,8 +11,7 @@ const Tuto = () => {
             <div className="col-sm-3 col-xs-12">
               <div className="album"><iframe width="560" height="315" className="img-responsive" src="https://www.youtube.com/embed/FC4qrLfLrGQ" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 <div className="albumdetail">
-                  {/* <h5>Crazy Freso</h5><a href="#" className="listen" data-toggle="modal" data-target="#albumdetail"><span */}
-                  {/* className="glyphicon glyphicon-headphones"></span> Listen Song</a> */}
+             
                 </div>
               </div>
             </div>
@@ -20,8 +19,7 @@ const Tuto = () => {
             <div className="col-sm-3 col-xs-12">
               <div className="album"><iframe width="560" height="315" className="img-responsive" src="https://www.youtube.com/embed/UAHw7OS-vkI" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 <div className="albumdetail">
-                  {/* <h5>Crazy Freso</h5><a href="#" className="listen" data-toggle="modal" data-target="#albumdetail"><span */}
-                  {/* className="glyphicon glyphicon-headphones"></span> Listen Song</a> */}
+          
                 </div>
               </div>
             </div>
@@ -29,8 +27,7 @@ const Tuto = () => {
             <div className="col-sm-3 col-xs-12">
               <div className="album"><iframe width="560" height="315" className="img-responsive" src="https://www.youtube.com/embed/rknLXpKqYEI" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 <div className="albumdetail">
-                  {/* <h5>Crazy Freso</h5><a href="#" className="listen" data-toggle="modal" data-target="#albumdetail"><span */}
-                  {/* className="glyphicon glyphicon-headphones"></span> Listen Song</a> */}
+         
                 </div>
               </div>
             </div>
@@ -38,8 +35,7 @@ const Tuto = () => {
             <div className="col-sm-3 col-xs-12">
               <div className="album"><iframe width="560" height="315" className="img-responsive"src="https://www.youtube.com/embed/6HVX2zH9CEw" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 <div className="albumdetail">
-                  {/* <h5>Crazy Freso</h5><a href="#" className="listen" data-toggle="modal" data-target="#albumdetail"><span
-                    className="glyphicon glyphicon-headphones"></span> Listen Song</a> */}
+                
                 </div>
               </div>
             </div>
@@ -47,8 +43,7 @@ const Tuto = () => {
             <div className="col-sm-3 col-xs-12">
               <div className="album"><iframe width="560" height="315" className="img-responsive" src="https://www.youtube.com/embed/flUkXJpD270" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 <div className="albumdetail">
-                  {/* <h5>Crazy Freso</h5><a href="#" className="listen" data-toggle="modal" data-target="#albumdetail"><span
-                    className="glyphicon glyphicon-headphones"></span> Listen Song</a> */}
+             
                 </div>
               </div>
             </div>
@@ -56,8 +51,7 @@ const Tuto = () => {
             <div className="col-sm-3 col-xs-12">
               <div className="album"><iframe width="560" height="315" className="img-responsive" src="https://www.youtube.com/embed/UmyVEJOZE-s" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 <div className="albumdetail">
-                  {/* <h5>Crazy Freso</h5><a href="#" className="listen" data-toggle="modal" data-target="#albumdetail"><span
-                    className="glyphicon glyphicon-headphones"></span> Listen Song</a> */}
+                
                 </div>
               </div>
             </div>
@@ -65,8 +59,7 @@ const Tuto = () => {
             <div className="col-sm-3 col-xs-12">
               <div className="album"><iframe width="560" height="315" className="img-responsive" src="https://www.youtube.com/embed/8n7lgQD2koo" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 <div className="albumdetail">
-                  {/* <h5>Crazy Freso</h5><a href="#" className="listen" data-toggle="modal" data-target="#albumdetail"><span
-                    className="glyphicon glyphicon-headphones"></span> Listen Song</a> */}
+                 
                 </div>
               </div>
             </div>
@@ -74,8 +67,7 @@ const Tuto = () => {
             <div className="col-sm-3 col-xs-12">
               <div className="album"><iframe width="560" height="315" className="img-responsive" src="https://www.youtube.com/embed/uis99Zz-5DU" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 <div className="albumdetail">
-                  {/* <h5>Crazy Freso</h5><a href="#" className="listen" data-toggle="modal" data-target="#albumdetail"><span
-                    className="glyphicon glyphicon-headphones"></span> Listen Song</a> */}
+                
                 </div>
               </div>
             </div>
--- psychedelic/advogadsheroku/src/pages/_document.js
+++ psychedelic/advogadsheroku/src/pages/_document.js
@@ -9,62 +9,8 @@ class MyDocument extends Document {
   render () {
     return (
       <Html>
-        <Head>
-          <title>Psychedeliclab</title>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-CTY634NDSF"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());        
-              gtag('config', 'G-CTY634NDSF');
-            </script>
-              `
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-186277147-1">
-            </script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'UA-186277147-1');
-            </script>
-              `
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              <script data-ad-client="ca-pub-7405864963958025" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-              `
-            }}
-          />
-
-          <meta charSet={'utf-8'} />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="language" content="pt-br, en" />
-          <link rel="shortcut icon" href="images/favicon.ico" type="favicon.ico" />
+         <Head>
         </Head>
-        <meta property="og:url" content="https://www.psychedeliclab.com.br" />
-        <meta name="og:title" content="Curso Darkpsy Psychedliclab" />
-         <meta property="og:image" content="https://www.psychedeliclab.com.br/darkpsy.jpeg" />
-        <meta name="og:site_name" content="Psychedelic labb" />
-        <meta name="fb:page_id" content="Psychedelic lab" />
-        <meta property="og:type" content="Course" />
-        <meta name="og:description" content="Darkpsy" />
-        <meta name="description" content="Psychedelic lab, aprenda a produzir Darkpsy com a primeira plataforma de cursos brasileira focada na vertente." />
-        <meta property="og:description" 
-              content="Do zero ao profissional" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="Darkpsy course" />
-        <meta name="twitter:image" content="https://www.psychedeliclab.com.br/darkpsy.jpeg" />
-        <meta name="twitter:site" content="@asd" />
 
         <body>
           <Main />
