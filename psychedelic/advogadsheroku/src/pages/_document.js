42a448c106fdd1a9b499283e8180bafcd673f2b3
Lumaraescoder <mephsluh@gmail.com>
2021-01-25 02:30:34 -0500

    ana22

--- psychedelic/advogadsheroku/src/pages/_document.js
+++ psychedelic/advogadsheroku/src/pages/_document.js
@@ -10,12 +10,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta charSet={'utf-8'} />
+          {/* <meta charSet={'utf-8'} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="language" content="pt-br, en" />
-          <link rel="shortcut icon" href="images/favicon.ico" type="favicon.ico" />
+          <link rel="shortcut icon" href="images/favicon.ico" type="favicon.ico" /> */}
         </Head>
-        <meta property="og:url" content="https://www.psychedeliclab.com.br" />
+        {/* <meta property="og:url" content="https://www.psychedeliclab.com.br" />
         <meta name="og:title" content="Curso Darkpsy Psychedliclab" />
          <meta property="og:image" content="https://www.psychedeliclab.com.br/darkpsy.jpeg" />
         <meta name="og:site_name" content="Psychedelic labb" />
@@ -28,28 +28,12 @@ class MyDocument extends Document {
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="Darkpsy course" />
         <meta name="twitter:image" content="https://www.psychedeliclab.com.br/darkpsy.jpeg" />
-        <meta name="twitter:site" content="@asd" />
+        <meta name="twitter:site" content="@asd" /> */}
 
         <body>
           <Main />
-          <NextScript>
-          <script data-ad-client="ca-pub-7405864963958025" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y6FBN7CV25"></script>
-          
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-Y6FBN7CV25');
-            </script>
-              `
-            }}
-          />
+          <NextScript />
 
-          </NextScript>
         </body>
       </Html>
     )
