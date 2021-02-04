a67fdfc18d4596e96f714f226f9afd6f6754e81e
lumaraescoder <mephsluh@gmail.com>
2021-02-04 13:14:54 -0500

    pixel3

--- psychedelic/advogadsheroku/src/pages/_document.js
+++ psychedelic/advogadsheroku/src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { FB_PIXEL_ID } from '../../utils/fpixel'
 
 class MyDocument extends Document {
   static async getInitialProps (ctx) {
@@ -8,7 +9,6 @@ class MyDocument extends Document {
 
   render () {
     const GA_MEASUREMENT_ID = 'G-CTY634NDSF'; // Paste your GTAG here
-    const  FA_CODE_ID = '282337049275712';
 
 
     return (
@@ -33,27 +33,28 @@ class MyDocument extends Document {
             }}
           />
            <script
-            // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{
               __html: `
-              <script>
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${FA_CODE_ID}');
-              fbq('track', 'PageView');
-              </script>
-              <noscript><img height="1" width="1" style="display:none"
-              src="https://www.facebook.com/tr?id=${FA_CODE_ID}&ev=PageView&noscript=1"
-              /></noscript>
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', ${FB_PIXEL_ID});
               `,
             }}
           />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            />
+          </noscript>
 
           
         </Head>
