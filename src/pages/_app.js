8afe425a07d33f274e32e94211f5735b11abd0b7
abner1902 <70659813+abner1902@users.noreply.github.com>
2026-05-19 15:49:40 -0300

    perf: optimization of scripts and accessibility viewport

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,43 +1,53 @@
+import Head from 'next/head'
+import Script from 'next/script'
+import { DefaultSeo } from 'next-seo'
 import Footer from '../components/Footer'
 import Preloader from '../components/Preloader'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { LayoutGlobalStyles } from '../styles/LayoutGlobalStyles'
+import config from '../components/config/seo.json'
+
+// Estilos Globais
+import 'bootstrap/dist/css/bootstrap.min.css'
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import '../style.css'
-import { LayoutGlobalStyles } from '../styles/LayoutGlobalStyles'
-import Head from 'next/head'
-import { DefaultSeo } from 'next-seo'
-import config from '../components/config/seo.json'
 
 const MyApp = ({ Component, pageProps }) => ( 
-    <div>
+    <>
         <LayoutGlobalStyles />
         <Preloader />
         <DefaultSeo {...config} />
 
-         <Head>
-
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
-            <meta charSet={'utf-8'} />
+        <Head>
+            {/* CORREÇÃO DE ACESSIBILIDADE: Permite zoom no mobile (exigência Lighthouse) */}
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta charSet="utf-8" />
             <meta property="og:image" content="https://www.psychedeliclab.com.br/darkpsy.webp" />
             <meta name="language" content="pt-br, en" />
-            <link rel="shortcut icon" href="images/favicon.ico" type="favicon.ico" />
-          </Head>
+            <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
+        </Head>
+
+        {/* FACEBOOK PIXEL OTIMIZADO: Carrega em segundo plano sem travar o First Paint */}
+        <Script id="fb-pixel" strategy="lazyOnload">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              
+              fbq('init', '282337049275712'); // Módulo 2
+              fbq('init', '710855122954203'); // Módulo 1
+              fbq('track', 'PageView');
+            `}
+        </Script>
+
         <Component {...pageProps} />
         <Footer/>
-    </div>  
+    </>
 )
   
-  // Only uncomment this method if you have blocking data requirements for
-  // every single page in your application. This disables the ability to
-  // perform automatic static optimization, causing every page in your app to
-  // be server-side rendered.
-  //
-  // MyApp.getInitialProps = async (appContext) => {
-  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
-  //   const appProps = await App.getInitialProps(appContext);
-  //
-  //   return { ...appProps }
-  // }
-  
-  export default MyApp
+export default MyApp
